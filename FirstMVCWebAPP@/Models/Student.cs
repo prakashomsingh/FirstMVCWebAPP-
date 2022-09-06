9a81c800e363d83d6d5b8f4953c0ab8bc350df5a
Sys1 <Sys1@SEDULOUS1>
2022-09-06 18:12:39 +0530

    Add project files.

---
+++ FirstMVCWebAPP@/Models/Student.cs
@@ -0,0 +1,13 @@
+﻿namespace FirstMVCWebAPP_.Models
+{
+    public class Student
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string City { get; set; }
+
+
+    }
+}
